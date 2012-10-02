5673cc037ae980ac60c2f7258432ce15f31faa5e
Tomasz Dziurko <tdziurko@gmail.com>
2012-10-02 12:39:26 +0200

    Fixed not needed import

--- itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/locations/RemoveLocationLink.java
+++ itemdirectory.webapplication/src/main/java/pl/tomaszdziurko/itemdirectory/web/view/locations/RemoveLocationLink.java
@@ -6,7 +6,6 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import pl.tomaszdziurko.itemdirectory.domain.entities.items.Location;
 import pl.tomaszdziurko.itemdirectory.service.items.LocationService;
-import sun.jdbc.odbc.ee.ObjectPool;
 
 public class RemoveLocationLink extends Link<Void> {
 	
